42d72c542762a57cb7590e267eda47f28ffb0fe2
paparazyzz <willykrizer@gmail.com>
2024-06-11 19:13:39 +0700

    Add files via upload

---
+++ JokiMandra/contact.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact | Us</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <!-- CSS -->
+    <link rel="stylesheet" href="contact.css">
+    <!-- Icons -->
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+<body>
+    <header>
+        <a href="" class="logo"> Waterbom <span>Bali</span></a>
+        <ul class="navbar">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="#">Facilities</a></li>
+            <li><a href="">Location</a></li>
+            <li><a href="#">Contact Us</a></li>
+        </ul>
+    </header>
+    <!-- Card Profile -->
+    <section class="exp" id="exp">
+      <div class="box">
+      <div class="text">
+            <h2>Contact Us</h2>
+            <p>For more information</p>
+        </div>
+        <div class="wrapper">
+          <div class="exp-item">
+            <img src="asset/9720038.jpg" />
+            <h2>Amanda Devira Melviana</h2>
+            <p>
+            <i class='bx bx-envelope'></i> : mandamelvinn@gmail.com
+            <br>
+            <i class='bx bx-phone-call' ></i> : 0851 6161 7173
+            </p>
+          </div>
+          <div class="exp-item">
+            <img src="asset/9720009.jpg" />
+            <h2>Steven Alvin</h2>
+            <p>
+            <i class='bx bx-envelope'></i> :  salvinx15x@gmail.com
+            <br>
+            <i class='bx bx-phone-call' ></i> : 0821 1252 5054
+            </p>
+          </div>
+          <div class="exp-item">
+            <img src="asset/9720009.jpg" />
+            <h2>Carmenita Rise Anggoro</h2>
+            <p>
+            <i class='bx bx-envelope'></i> : carmenitanggoro@gmail.com
+            <br>
+            <i class='bx bx-phone-call' ></i> : 0812 9211 0535
+            </p>
+          </div>
+          <div class="exp-item">
+            <img src="asset/9720038.jpg" />
+            <h2>Ariel Cherlindrea</h2>
+            <p>
+            <i class='bx bx-envelope'></i> :  arielcherlins@gmail.com
+            <br>
+            <i class='bx bx-phone-call' ></i> : 0855 1788 887
+            </p>
+          </div>
+
+        </div>
+      </div>
+    </section>
+</body>
+</html>
